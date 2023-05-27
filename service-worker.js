183332abbf1da2546b1bf4a8070eb12596305007
unknown <jose.ortiz1239@gmail.com>
2023-05-27 00:01:15 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.1ae225207b554a60c3ff9a2c5441a036.js"
+  "JoseOrtiz1239.github.io/portfolio/precache-manifest.0f1fe45327a0cb0ce529c5cadbdf15bd.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/portfolio/index.html", {
+workbox.routing.registerNavigationRoute("JoseOrtiz1239.github.io/portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
