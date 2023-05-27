cb22ffa9fcd045a39629962a112d4a0922093c3e
unknown <jose.ortiz1239@gmail.com>
2023-05-26 23:52:35 -0500

    Updates

---
+++ precache-manifest.1ae225207b554a60c3ff9a2c5441a036.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9e5094f52d68634ca59",
+    "url": "/portfolio/static/css/main.3590f8a7.chunk.css"
+  },
+  {
+    "revision": "a9e5094f52d68634ca59",
+    "url": "/portfolio/static/js/main.a9e5094f.chunk.js"
+  },
+  {
+    "revision": "410c39463b762bcb0ea3",
+    "url": "/portfolio/static/css/1.900f5b29.chunk.css"
+  },
+  {
+    "revision": "410c39463b762bcb0ea3",
+    "url": "/portfolio/static/js/1.410c3946.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "23273bc8bc3197e29e433d7cf66185fc",
+    "url": "/portfolio/static/media/sklearn_genetic_opt.23273bc8.png"
+  },
+  {
+    "revision": "fed0850a36ae8cfbe9ceaa1ce970c5cc",
+    "url": "/portfolio/static/media/graph_embeddings.fed0850a.png"
+  },
+  {
+    "revision": "9c0f986795edbdc96c99bc8853580310",
+    "url": "/portfolio/static/media/kafkaml.9c0f9867.png"
+  },
+  {
+    "revision": "8ba027d94d0cd4d31608e2c6e43f9488",
+    "url": "/portfolio/static/media/scikit_pipes.8ba027d9.png"
+  },
+  {
+    "revision": "4b632d51a45152af9b70077a3d4955ca",
+    "url": "/portfolio/static/media/portfolio.4b632d51.png"
+  },
+  {
+    "revision": "65b1aa5452e07609b57626b4574bdaa0",
+    "url": "/portfolio/index.html"
+  }
+];
